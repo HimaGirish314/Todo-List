a12e0f9047ebfb7755e20d0ce3e04926c99d5e3f
HimaGirish314 <Himagirish.bandi@gmail.com>
2023-01-23 12:40:28 +0530

    added local storage

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,46 +1,51 @@
-import React,{useState} from 'react';
+import React,{useState,useEffect} from 'react';
 import TodoList from './TodoList';
 
 const Todo = () => {
-const [inputTodo,setInputTodo]=useState('')
-const [editTodo,setEditTodo]=useState('')
-const [todos,setTodos]=useState([])
+    const initialState=JSON.parse(localStorage.getItem("todos")) || [];
+    const [inputTodo,setInputTodo]=useState('')
+    const [editTodo,setEditTodo]=useState('')
+    const [todos,setTodos]=useState(initialState)
 
-console.log(editTodo)
-const handleSubmit=(e)=>{
-    e.preventDefault()
-    if(todos.includes(editTodo)){
-        editTodo.name=inputTodo
-        setEditTodo('')
-        setInputTodo('');
-        return 
+    useEffect(()=>{
+        localStorage.setItem("todos", JSON.stringify(todos))
+    },[todos])
+
+    console.log(editTodo)
+    const handleSubmit=(e)=>{
+        e.preventDefault()
+        if(todos.includes(editTodo)){
+            editTodo.name=inputTodo
+            setEditTodo('')
+            setInputTodo('');
+            return 
+        }
+        else{
+        setTodos([...todos,{id:todos.length+1,name:inputTodo}])
+        setInputTodo('')
+        }
+        
     }
-    else{
-    setTodos([...todos,{id:todos.length+1,name:inputTodo}])
-    setInputTodo('')
+    const handleDelete=(data)=>{
+        setTodos(todos.filter((res)=>{
+        return res.id !== data.id
+    }))
     }
-    
-}
-const handleDelete=(data)=>{
-    setTodos(todos.filter((res)=>{
-    return res.id !== data.id
- }))
-}
-const handleEdit=(data)=>{
-    setEditTodo(data)
-    setInputTodo(data.name)
-}
-  return (
-    <div className="container">
-        <div className="inputData">
-            <form onSubmit={handleSubmit}>
-            <input type="text" placeholder='Enter Here....' value={inputTodo} onChange={(e)=>{setInputTodo(e.target.value)}}/>
-            <button type='submit'>{editTodo ?"Update" :"Add"}</button>
-            </form>
+    const handleEdit=(data)=>{
+        setEditTodo(data)
+        setInputTodo(data.name)
+    }
+    return (
+        <div className="container">
+            <div className="inputData">
+                <form onSubmit={handleSubmit}>
+                <input type="text" placeholder='Enter Here....' value={inputTodo} onChange={(e)=>{setInputTodo(e.target.value)}}/>
+                <button type='submit'>{editTodo ?"Update" :"Add"}</button>
+                </form>
+            </div>
+            <TodoList  todos={todos} setTodos={setTodos} handleDelete={handleDelete} handleEdit={handleEdit}/>
         </div>
-        <TodoList  todos={todos} setTodos={setTodos} handleDelete={handleDelete} handleEdit={handleEdit}/>
-    </div>
-  )
-}
+    )
+    }
 
-export default Todo
+    export default Todo
