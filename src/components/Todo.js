5f5f4482713c2c83ce122edb999c476f3af79100
HimaGirish314 <Himagirish.bandi@gmail.com>
2023-01-23 12:31:30 +0530

    Edit functionality added

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,26 +3,42 @@ import TodoList from './TodoList';
 
 const Todo = () => {
 const [inputTodo,setInputTodo]=useState('')
+const [editTodo,setEditTodo]=useState('')
 const [todos,setTodos]=useState([])
+
+console.log(editTodo)
 const handleSubmit=(e)=>{
     e.preventDefault()
+    if(todos.includes(editTodo)){
+        editTodo.name=inputTodo
+        setEditTodo('')
+        setInputTodo('');
+        return 
+    }
+    else{
     setTodos([...todos,{id:todos.length+1,name:inputTodo}])
     setInputTodo('')
+    }
+    
 }
 const handleDelete=(data)=>{
     setTodos(todos.filter((res)=>{
     return res.id !== data.id
  }))
 }
+const handleEdit=(data)=>{
+    setEditTodo(data)
+    setInputTodo(data.name)
+}
   return (
     <div className="container">
         <div className="inputData">
             <form onSubmit={handleSubmit}>
             <input type="text" placeholder='Enter Here....' value={inputTodo} onChange={(e)=>{setInputTodo(e.target.value)}}/>
-            <button type='submit'>Add</button>
+            <button type='submit'>{editTodo ?"Update" :"Add"}</button>
             </form>
         </div>
-        <TodoList  todos={todos} setTodos={setTodos} handleDelete={handleDelete}/>
+        <TodoList  todos={todos} setTodos={setTodos} handleDelete={handleDelete} handleEdit={handleEdit}/>
     </div>
   )
 }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
-const TodoList = ({todos,handleDelete}) => {
+const TodoList = ({todos,handleDelete,handleEdit}) => {
   return (
         <div className="displayTodo">
             { todos.length > 0 &&
               todos.map((data ,ind)=>{
                 return (
                   <div key={ind}>
-                  <h3>{data.name} <button>Edit</button> <button onClick={()=>handleDelete(data)}>Delete</button></h3>
+                  <h3>{data.name} <button onClick={()=>{handleEdit(data)}}>Edit</button> <button onClick={()=>handleDelete(data)}>Delete</button></h3>
                   </div>
                 )
               })
